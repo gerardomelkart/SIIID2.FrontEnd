052e9e41bd3490f55b4454432ec17dd86227bc00
gerardomelkart <38387561+gerardomelkart@users.noreply.github.com>
2026-06-25 18:14:13 -0600

    Prueba decorador angular

--- src/app/pages/originales/originales.ts
+++ src/app/pages/originales/originales.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+@Component({
+  selector: 'app-originales',
+  templateUrl: './originales.html',
+})
 export class Originales {
   titulo = 'Originales';
 }
