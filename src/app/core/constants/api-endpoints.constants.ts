116129c7870e4675f31f43c4a9e1b61b871f0e1d
gerardomelkart <gerardomelkart@gmail.com>
2026-09-07 14:10:35 -0600

    actualizacion federal 2

--- src/app/core/constants/api-endpoints.constants.ts
+++ src/app/core/constants/api-endpoints.constants.ts
@@ -18,6 +18,7 @@ export const API_ENDPOINTS = {
   semanalArchivosOriginales: `${API_BASE_URL}/semanal/informes/archivos-originales`,
   federalCatalogos: `${API_BASE_URL}/federal/catalogos`,
   federalCargas: `${API_BASE_URL}/federal/cargas`,
+  federalActualizaciones: `${API_BASE_URL}/federal/actualizaciones`,
   federalAdministracionCargas: `${API_BASE_URL}/federal/administracion/cargas-pendientes`,
   federalInformes: `${API_BASE_URL}/federal/informes`,
   notificacionesRechazos: `${API_BASE_URL}/notificaciones/rechazos`,
